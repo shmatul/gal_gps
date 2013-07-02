126890646c58f817f4b20d622296e8654123171d
Gal Weissman <shmatul@gmail.com>
2013-07-02 09:02:47 +0300

    again

--- js/controllers.js
+++ js/controllers.js
@@ -1,29 +1,5 @@
 function Welcome($scope){
-
     
-    document.addEventListener('deviceready', function() {
-                              try {      
-                                  console.log(FB);
-                              FB.init({ appId: "678702565479891", nativeInterface: CDV.FB, useCachedDialogs: false });
-                                  
-                              } catch (e) {
-                              alert(e);
-                              }
-                              }, false);
     
-    FB.Event.subscribe('auth.login', function(response) {
-                       alert('auth.login event');
-                       });
-    
-    FB.Event.subscribe('auth.logout', function(response) {
-                       alert('auth.logout event');
-                       });
-    
-    FB.Event.subscribe('auth.sessionChange', function(response) {
-                       alert('auth.sessionChange event');
-                       });
-    
-    FB.Event.subscribe('auth.statusChange', function(response) {
-                       alert('auth.statusChange event');
-                       });
+ document.addEventListener('deviceready', function() { alert("heez") } );
 }
